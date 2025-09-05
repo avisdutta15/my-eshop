069a87b55cc40e18eb1d1f4f014105add83b63f3
Avishek Dutta <avishekdutta15@outlook.com>
2025-09-05 22:25:51 +0530

    Refactor Program class

--- catalog/src/MyEshop.Catalog.Api/Program.cs
+++ catalog/src/MyEshop.Catalog.Api/Program.cs
@@ -1,34 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
 
-namespace MyEshop.Catalog.Api
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
